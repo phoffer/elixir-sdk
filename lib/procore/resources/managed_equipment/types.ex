5c212d942062ae9f2c30bd97b151ccc31f27f84b
Paul Hoffer <git@paulhoffer.com>
2019-09-02 22:59:43 -0700

    fix docs for equipment types

--- lib/procore/resources/managed_equipment/types.ex
+++ lib/procore/resources/managed_equipment/types.ex
@@ -21,7 +21,7 @@ defmodule Procore.Resources.ManagedEquipment.Types do
   end
 
   @doc """
-  Gets an Equipment Make.
+  Gets an Equipment Type.
   """
   @spec find(Tesla.Client.t(), map) :: %ResponseResult{} | %ErrorResult{}
   def find(client, %{"company_id" => company_id, "id" => id}) do
@@ -32,7 +32,7 @@ defmodule Procore.Resources.ManagedEquipment.Types do
   end
 
   @doc """
-  Deletes an Equipment Make for a Company.
+  Deletes an Equipment Type for a Company.
   """
   @spec delete(Tesla.Client.t(), %{(company_id :: String.t()) => pos_integer}) ::
           %ResponseResult{} | %ErrorResult{}
@@ -44,7 +44,7 @@ defmodule Procore.Resources.ManagedEquipment.Types do
   end
 
   @doc """
-  Creates an Equipment Make.
+  Creates an Equipment Type.
   """
   @spec create(Tesla.Client.t(), map) :: %ResponseResult{} | %ErrorResult{}
   def create(client, %{"company_id" => company_id, "managed_equipment_type" => managed_equipment_type} = params) do
@@ -56,7 +56,7 @@ defmodule Procore.Resources.ManagedEquipment.Types do
   end
 
   @doc """
-  Updates an Equipment Make.
+  Updates an Equipment Type.
   """
   @spec update(Tesla.Client.t(), map) :: %ResponseResult{} | %ErrorResult{}
   def update(client, %{"company_id" => company_id, "managed_equipment_type" => %{"id" => id} = managed_equipment_type} = params) do
